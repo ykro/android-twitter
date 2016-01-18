ea527185144a730466f2e8877cd175be16623e47
ykro <aacs85@gmail.com>
2016-01-18 17:39:14 -0600

    Initial commit

---
+++ app/src/main/java/edu/galileo/android/twitterclient/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package edu.galileo.android.twitterclient.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.galileo.android.twitterclient.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
