065e55bb7ed98ef6f76f4c7cc0b73b7074a42424
ykro <aacs85@gmail.com>
2016-01-19 12:41:40 -0600

    adding login support

--- app/src/main/java/edu/galileo/android/twitterclient/login/LoginActivity.java
+++ app/src/main/java/edu/galileo/android/twitterclient/login/LoginActivity.java
@@ -1,14 +1,53 @@
 package edu.galileo.android.twitterclient.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
 
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import edu.galileo.android.twitterclient.R;
+import edu.galileo.android.twitterclient.images.ImagesActivity;
 
 public class LoginActivity extends AppCompatActivity {
+    @Bind(R.id.twitterLoginButton) TwitterLoginButton loginButton;
+    @Bind(R.id.loginContainer) RelativeLayout loginContainer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                navigateToMainScreen();
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                String msgError = String.format(getString(R.string.login_error_message), exception.getMessage());
+                Snackbar.make(loginContainer, msgError, Snackbar.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        loginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void navigateToMainScreen() {
+        startActivity( new Intent(this, ImagesActivity.class));
     }
 }
